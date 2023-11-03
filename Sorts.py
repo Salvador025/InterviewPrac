05163119b2163e6a29535d35fe57581b6c93a07b
Salvador025 <jose.mariscal@iteso.mx>
2023-11-03 13:18:02 -0600

    Selection terminado

--- Sorts.py
+++ Sorts.py
@@ -17,18 +17,32 @@ class Sorts:
 
     def Insertion(arr: list):
         # caso de i=0 inecesario
-        for i in range(len(arr)):
+        for i in range(1, len(arr)):
             for j in range(i,0,-1):
                 if arr[j] < arr[j-1]:
                     Sorts.Swap(arr,j,j-1)
                 else:
-                    break
+                    break    
 
-    def Selection(arr:list):
-        for i in range(len(arr)):
-            pass
-                    
-                    
+    def Selection(arr: list):
+        for i in range(len(arr)-1):
+
+            menor = i
+
+            for j in range(i+1,len(arr)):
+
+                if arr[j] < arr[menor]:
+                    menor = j
+
+            if menor != i:
+                Sorts.Swap(arr, menor, i)
+                     
+
+               
+
+
+
+
 
 """
     optimizacion de Insertion: menos movimientos
@@ -42,6 +56,8 @@ class Sorts:
             arr[j] = pivot
  """
 
+
+
 if __name__ == "__main__":
     """
     test bubble sort
@@ -51,7 +67,15 @@ if __name__ == "__main__":
     print(arr)
     """
 
+    '''
+    test Selection sort
     arre = [9, 4, 6, 8, 5, 7, 1, 2, 3]
     print(arre)
     Sorts.Insertion(arre)
+    print(arre)
+    '''
+
+    arre = [9, 4, 6, 8, 4, 7, 1, 2, 3]
+    print(arre)
+    Sorts.Selection(arre)
     print(arre)
