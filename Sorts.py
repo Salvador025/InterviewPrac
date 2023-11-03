94e279d02db101d83ece74908829579040484733
joseCarlosAc <jose.acevesgon@gmail.com>
2023-11-02 19:01:02 -0600

    comentarios de insertion sort y segunda opcion

--- Sorts.py
+++ Sorts.py
@@ -1,36 +1,51 @@
-class Sorts():
-
-    def Swap(arr:list, i:int, j: int):
+# cspell: disable
+class Sorts:
+    def Swap(arr: list, i: int, j: int):
         temp = arr[i]
         arr[i] = arr[j]
         arr[j] = temp
 
-    def BubbleSort(arr:list):
-        for i in range(len(arr)-1):
+    def BubbleSort(arr: list):
+        for i in range(len(arr) - 1):
             flag = True
-            for j in range(len(arr)-i-1):
-                if arr[j]>arr[j+1]:
-                    Sorts.Swap(arr, j, j+1)
+            for j in range(len(arr) - i - 1):
+                if arr[j] > arr[j + 1]:
+                    Sorts.Swap(arr, j, j + 1)
                     flag = False
-            if flag: 
+            if flag:
                 break
 
-    def Insertion(arr:list):
+    def Insertion(arr: list):
+        # caso de i=0 inecesario
         for i in range(len(arr)):
-            for j in range(i,0,-1):
-                if arr[j] < arr[j-1]:
-                    Sorts.Swap(arr,j,j-1)
+            for j in range(i, 0, -1):
+                if arr[j] < arr[j - 1]:
+                    Sorts.Swap(arr, j, j - 1)
+                # optimiza con un else
 
 
+"""
+    optimizacion de Insertion: menos movimientos
+    def Insertion(arr: list):
+        for i in range(1, len(arr)):
+            j = i
+            pivot = arr[i]
+            while j > 0 and pivot < arr[j - 1]:
+                arr[j] = arr[j - 1]
+                j -= 1
+            arr[j] = pivot
+ """
 
 if __name__ == "__main__":
-
-    arr=[9, 4, 6, 8, 5, 7, 1, 2, 3]
+    """
+    test bubble sort
+    arr = [9, 4, 6, 8, 5, 7, 1, 2, 3]
     print(arr)
     Sorts.BubbleSort(arr)
     print(arr)
+    """
 
-    arre=[9, 4, 6, 8, 5, 7, 1, 2, 3]
+    arre = [9, 4, 6, 8, 5, 7, 1, 2, 3]
     print(arre)
     Sorts.Insertion(arre)
     print(arre)
